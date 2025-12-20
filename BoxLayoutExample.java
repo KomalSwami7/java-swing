f582f26ee41df4c5bb49d7cb484c7cf308ab7eb1
KomalSwami7 <mayawandevaswami@gmail.com>
2025-12-20 14:17:18 +0000

    20/12

---
+++ BoxLayoutExample.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class BoxLayoutExample extends Frame
+{
+Button b[];
+
+
+    public BoxLayoutExample() {
+        b = new Button[5];
+        for (int i = 0; i < 5; i++) {
+            b[i] = new Button("Button" + (i + 1));
+            add(b[i]);
+        }
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setSize(500, 500);
+        setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        BoxLayoutExample b1 = new BoxLayoutExample();
+    }
+}
